d93cc914d90116c888c657a462b1d5babbf7e574
Jorpeser <pepeperezvalenzuela@gmail.com>
2023-07-07 17:17:31 +0200

    Logo + App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+import PatientsPage from "./pages/PatientsPage";
+import InventoryPage from "./pages/InventoryPage";
+import HomePage from "./pages/HomePage";
+import SchedulePage from "./pages/SchedulePage";
+import ReportsPage from "./pages/ReportsPage";
+
+const Wrapper = styled.div`
+  margin-top: 7rem;
+  padding: 2.4rem 3.2rem;
+`
 
 function App() {
   return (
     <>
-      Hola mundo
+      <Navbar />
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/patients" element={<PatientsPage />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </Wrapper>
     </>
   );
 }
